57099e65772af54dba335409ec48936a38c8295e
Sam Bautista <samuelbaubas@gmail.com>
2020-07-30 23:47:28 -0500

    ya pudimos observar coo se juntaría html con JS y que método es más funcional para que nuestra cotización se muestre de la mejor manera

--- JS/cotizacion.js
+++ JS/cotizacion.js
@@ -1,1 +1,5 @@
-console.log("si funciona")
+const a = 10,
+  b = 20;
+let suma = a+b
+document.getElementById("div").outerHTML = suma 
+coviene más el outerHTML por que hacemos que se ponga los elementos que queremos en el html sin más problemas listo
